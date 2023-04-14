95a8803e10d2a119bf4deffc1e502637862a6dd8
程镇 <2935691642@qq.com>
2023-04-14 21:57:14 +0800

    添加项目文件。

---
+++ StatisticalStatement.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace UpperConnection
+{
+    /// <summary>
+    /// StatisticalStatement.xaml 的交互逻辑
+    /// </summary>
+    public partial class StatisticalStatement : Window
+    {
+        public StatisticalStatement()
+        {
+            InitializeComponent();
+        }
+    }
+}
